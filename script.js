f1f21582ee22cad4e61b4a71f5da7f25e9f15a0f
YASHP1484 <yashp1484@gmail.com>
2024-04-02 18:28:18 +0530

    commit-changes

--- script.js
+++ script.js
@@ -2,39 +2,42 @@ let sideBar = document.getElementById("side");
 let main = document.getElementsByTagName("main");
 let sun = document.getElementById("sunrise");
 let p = document.getElementById("pblock");
-let span = document.getElementById("spann");
-console.log("yash");
+let span = document.getElementById("span");
+
 let openBar = document.createElement("i");
 openBar.classList.add("fa-solid", "fa-bars", "openbar");
-let search = document.getElementById("searchBar");
 
+let search = document.getElementById("searchBar");
 let serchDiv = document.getElementById("topSearchDiv");
+let spanCloseBtn;
+let span1;
+let inputval = [] || localStorage.getItem("inputVal");
+let store = [] || localStorage.getItem("div");
 
 function searchClick() {
-  var span1 = document.createElement("span");
-  span1.classList.add("closeBtn");
-  search.style.borderRadius = "0";
-  var text = document.createTextNode("\u00D7");
-  span1.appendChild(text);
-  serchDiv.appendChild(span1);
-  search.style.width = "540px";
+  if (!document.getElementById("closeBt")) {
+    span1 = document.createElement("span");
+    span1.classList.add("closeBtn");
+    span1.setAttribute("id", "closeBt");
+    search.style.borderRadius = "0";
+    let text = document.createTextNode("\u00D7");
+    span1.appendChild(text);
+    serchDiv.appendChild(span1);
+    search.style.width = "540px";
+  }
+  spanCloseBtn = document.querySelector("#closeBt");
+  spanCloseBtn.addEventListener("click", removeEle);
 }
-search.addEventListener("click", searchClick, { once: true });
+search.addEventListener("click", searchClick, {});
 
-let spanCloseBtn = document.querySelector(".closeBtn");
 function removeEle() {
   console.log("test");
   search.style.width = "200px";
-}
-if (spanCloseBtn) {
   console.log(spanCloseBtn);
-  spanCloseBtn.addEventListener("click", function () {
-    console.log("test");
-  });
-} else {
-  console.log("test3");
+  search.value = "";
+  serchDiv.removeChild(span1);
 }
-console.log("tesfsdfs");
+
 function closemenu() {
   sideBar.style.width = "0";
   sideBar.style.transition = "0.5s";
@@ -65,17 +68,21 @@ function createCopy() {
     createDiv.classList.add("main-list");
     content.appendChild(createDiv);
 
+    var response = document.createElement("div");
+    response.classList.add("getResponse");
+    createDiv.appendChild(response);
+
     var createLabel = document.createElement("label");
     createLabel.classList.add("task");
     createLabel.innerHTML = inputText.value;
-    createDiv.appendChild(createLabel);
+    response.appendChild(createLabel);
 
     var span = document.createElement("span");
     span.classList.add("span");
 
     var text = document.createTextNode("\u00D7");
     span.appendChild(text);
-    createLabel.appendChild(span);
+    response.appendChild(span);
 
     var createUl = document.createElement("ul");
     createUl.classList.add("icons");
@@ -104,15 +111,47 @@ function createCopy() {
     let createIc = document.createElement("i");
     createIc.classList.add("fa-solid", "fa-calendar-days");
     creatAc.appendChild(createIc);
+    inputval.push(createLabel.innerHTML);
+    let create = createDiv.innerHTML;
+    inputText.value = "";
+    if (!store.includes(create)) {
+      store.push(JSON.stringify(createDiv.innerHTML));
+      console.log(store);
+    }
   }
+  localStorage.setItem("inputVal", inputval);
+  localStorage.setItem("div", store);
 
   function handelClick(e) {
-    if ((e.target = createLabel)) {
+    if ((e.target = createLabel.innerHTML)) {
       createLabel.classList.toggle("checked");
+      createLabel.classList.toggle("label-checked-text");
+      completedTask();
     } else if ((inputText.value = "")) {
       alert("you must write something");
     }
   }
+  let task = document.getElementById("completedTaskCont");
+
+  function completedTask() {
+    // let heading = document.createElement("h3");
+    // heading.textContent = "completed";
+    // task.appendChild(heading);
+    let completeTaskDiv = document.createElement("div");
+    completeTaskDiv.classList.add("mainList");
+    task.appendChild(completeTaskDiv);
+
+    let resp = document.createElement("div");
+    resp.classList.add("getTextResponse");
+    completeTaskDiv.appendChild(resp);
+
+    let createLabelCompleteTask = document.createElement("label");
+    createLabelCompleteTask.classList.add("task-completed", "checked");
+    createLabelCompleteTask.textContent = createLabel.innerHTML;
+    resp.appendChild(createLabelCompleteTask);
+    createDiv.remove();
+  }
+
   function handle(e) {
     if ((e.target = span)) {
       createDiv.style.display = "none";
@@ -123,24 +162,26 @@ function createCopy() {
 }
 
 let groupEle = document.getElementById("group");
+let sortButn = document.getElementById("sortBtn");
+
 let rightBar = document.getElementById("right");
 let head = document.getElementById("closeSuges");
 
-var span1 = document.createElement("span");
-span1.classList.add("span");
+var span2 = document.createElement("span");
+span2.classList.add("span");
 
 var text1 = document.createTextNode("\u00D7");
-span1.appendChild(text1);
-head.appendChild(span1);
+span2.appendChild(text1);
+head.appendChild(span2);
 
 function rightSugestions() {
   rightBar.style.display = "block";
   rightBar.style.width = "40%";
-  groupEle.style.transition = "0.5s ease 2s";
 }
 groupEle.addEventListener("click", rightSugestions);
+sortButn.addEventListener("click", rightSugestions);
 
 function close() {
   rightBar.style.display = "none";
 }
-span1.addEventListener("click", close);
+span2.addEventListener("click", close);
